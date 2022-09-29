8c76ab036080c089b347c43c857e168335053a4d
nishanth iruthayaraj <nishanthiruthayaraj@gmail.com>
2022-09-30 01:23:09 +0200

    updated code

--- sql_queries.py
+++ sql_queries.py
@@ -36,17 +36,17 @@ time_table_create = ("CREATE TABLE IF NOT EXISTS time(\
 songplay_table_insert = ("INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, \
                              session_id, location, user_agent) \
                              VALUES (%s,%s,%s,%s,%s,%s,%s,%s) \
-                             ON CONFLICT (songplay_id)\
+                             ON CONFLICT(songplay_id) \
                              DO NOTHING")
 
 user_table_insert = ("INSERT INTO users (user_id, first_name, last_name, gender, level) \
                             VALUES (%s,%s,%s,%s,%s) \
-                            ON CONFLICT (user_id) \
-                            DO NOTHING")
+                            ON CONFLICT(user_id) \
+                            DO UPDATE SET level = excluded.level")
 
 song_table_insert = ("INSERT INTO songs (song_id, title, artist_id, year, duration) \
                             VALUES (%s,%s,%s,%s,%s) \
-                            ON CONFLICT \
+                            ON CONFLICT (song_id) \
                             DO NOTHING")
 
 artist_table_insert = ("INSERT INTO artists (artist_id, name, location, latitude, longitude) \
